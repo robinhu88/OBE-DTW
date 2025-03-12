ae9c8a85b62788fa457a2af8b668b21956bf5344
Robin Hu <2900404327@qq.com>
2025-03-12 10:46:51 +0800

    1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool, cpu_count
 import os
 import pandas as pd
+import math
 
 
 class ObeDtwCalculator:
@@ -60,9 +61,10 @@ def read_data(file_path):
     return mag_data, xy_coords
 
 
-def create_windows(data, window_size=12):
+def create_windows(data, window_size=100):
     windows = []
-    for i in range(len(data) - window_size + 1):
+    # 使用步长10来生成窗口
+    for i in range(0, len(data) - window_size + 1, 10):
         window = data[i:i + window_size]
         windows.append(window)
         # if i % 100 == 0:
@@ -107,32 +109,61 @@ def plot_sequences(matches, test_end_points, title="", output_dir="."):
     plt.close(fig)
     plt.show()
 
+
 def calculate_errors(errors):
     mae = np.mean(errors)
-    mse = np.mean(np.square(errors))
-    rmse = np.sqrt(mse)
+    sorted_errors = sorted(errors)
+    lower_bound_index = math.ceil(len(sorted_errors) * 0.165)
+    error67 = sorted_errors[lower_bound_index]
 
-    return mae, mse, rmse
+    return mae, error67
+
+
+def calculate_cumulative_distances(xy_coordinates):
+    cumulative_distances = [0]
+    total_distance = 0
+    for i in range(1, len(xy_coordinates)):
+        distance = np.linalg.norm(np.array(xy_coordinates[i]) - np.array(xy_coordinates[i - 1]))
+        total_distance += distance
+        cumulative_distances.append(total_distance)
+    return cumulative_distances
+
+
 if __name__ == "__main__":
-    map_file = r'C:\Users\Administrator\Desktop\data\2map155531.txt'
-    test_files = [ r'C:\Users\Administrator\Desktop\data\data153344.txt',
-        r'C:\Users\Administrator\Desktop\data\data154324.txt',
-        r'C:\Users\Administrator\Desktop\data\data161152.txt',
-        r'C:\Users\Administrator\Desktop\data\data162030.txt',
-        r'C:\Users\Administrator\Desktop\data\data163356.txt',
-        r'C:\Users\Administrator\Desktop\data\data164715.txt',
-        r'C:\Users\Administrator\Desktop\data\data165807.txt',
-        r'C:\Users\Administrator\Desktop\data\data171421.txt',
-        r'C:\Users\Administrator\Desktop\data\data172624.txt']
+    map_file = r'C:\Users\Administrator\Desktop\data100Hz\map155531.txt'
+    test_files = [
+
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata153344.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata154324.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata161152.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata162030.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata163356.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata164715.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata165807.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata171421.txt',
+        r'C:\Users\Administrator\Desktop\data10Hz\10hzdata172624.txt'
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata153344.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata154324.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata155531.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata161152.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata162030.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata163356.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata164715.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata165807.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata171421.txt',
+        # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata172624.txt'
+    ]
 
     # 读取基准地图文件
     map_mag, map_xy = read_data(map_file)
 
     dtw_calculator = ObeDtwCalculator()
+    all_errors = []
 
     for test_file in test_files:
         all_test_xy_coordinates = []
         all_matches = []
+        all_cumulative_distances = []
 
         test_mag, test_xy = read_data(test_file)
         test_mag_windows = create_windows(test_mag)
@@ -148,22 +179,24 @@ if __name__ == "__main__":
 
         matches = []
         errors = []
+        cumulative_errors = []
         for result, test_coord in zip(results, all_test_xy_coordinates):
             best_match_point, best_match_index = result
-            #print(f"Test Coordinate: {test_coord}, Best Match Point: {best_match_point}")
+            # print(f"Test Coordinate: {test_coord}, Best Match Point: {best_match_point}")
             error = np.linalg.norm(np.array(test_coord) - np.array(best_match_point))
             # 输出误差值以便检查
-            #print(f"Calculated Error: {error}")
+            # print(f"Calculated Error: {error}")
             matches.append((best_match_point, error))
             errors.append(error)
 
-        avg_mae, avg_mse, avg_rmse = calculate_errors(errors)
+        all_errors.extend(errors)
 
+        avg_mae, error67 = calculate_errors(errors)
+        cumulative_distances = calculate_cumulative_distances(all_test_xy_coordinates)
+        all_cumulative_distances.extend(cumulative_distances)
         # 输出误差
-        print(f"File: {test_file}, MAE: {avg_mae:.4f}, MSE: {avg_mse:.4f}, RMSE: {avg_rmse:.4f}")
+        print(f"File: {test_file}, MAE: {avg_mae:.4f}, 67%Error: {error67:.4f}")
         all_matches.extend(matches)
-
-
 
         # 输出每个测试文件的最佳匹配点和误差
         # for idx, (point, error) in enumerate(matches):
@@ -173,6 +206,56 @@ if __name__ == "__main__":
 
         plot_sequences(matches, all_test_xy_coordinates, title=f"Matches for {os.path.basename(test_file)}",
                        output_dir="output_images_12")
+        # 绘制累积里程与误差的关系图
+        cumulative_errors = [0]
+        for i in range(1, len(errors)):
+            cumulative_error = cumulative_distances[i - 1] + errors[i]
+            cumulative_errors.append(cumulative_error)
+        fig, ax = plt.subplots()
+        ax.plot(cumulative_distances, errors, 'o', markersize=2, alpha=0.5)
+        plt.xlabel('Cumulative Distance (m)')
+        plt.ylabel('Cumulative Error (m)')
+        plt.title(f'Error vs Cumulative Distance for {os.path.basename(test_file)}')
+        output_dir = "output_images"
+        os.makedirs(output_dir, exist_ok=True)
+        output_file = os.path.join(output_dir,
+                                   f"cumulative_error_vs_cumulative_distance_{os.path.basename(test_file)}.png")
+        plt.savefig(output_file)
+        plt.close(fig)
+    # 计算总的平均值和第67百分位的值
+    total_avg_mae = np.mean(all_errors)
+    all_errors_sorted = np.sort(all_errors)
+    total_error67 = all_errors_sorted[int(0.67 * len(all_errors_sorted))]
 
+    print(f"Total MAE: {total_avg_mae:.4f}, Total 67% Error: {total_error67:.4f}")
 
-
+#test_files = [
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata153344.txt',
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata154324.txt',
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata161152.txt'
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata162030.txt',
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata163356.txt',
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata164715.txt',
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata165807.txt',
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata171421.txt',
+    # r'C:\Users\Administrator\Desktop\data100Hz\100hzdata172624.txt'
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata153344.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata154324.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata161152.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata155531.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata162030.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata163356.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata164715.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata165807.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata171421.txt',
+    # r'C:\Users\Administrator\Desktop\data10Hz\10hzdata172624.txt'
+    # r'C:\Users\Administrator\Desktop\data1Hz\data153344.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data154324.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data161152.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data162030.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data163356.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data164715.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data165807.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data171421.txt',
+    # r'C:\Users\Administrator\Desktop\data1Hz\data172624.txt'
+#]
